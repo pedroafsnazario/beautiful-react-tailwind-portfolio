6456002df71e1abe97735b2aa9c9ef3778d40955
Pedro Antonio <pedro905fernandes@gmail.com>
2025-05-13 00:44:46 -0300

    first commit

---
+++ src/components/ProjectsSection.jsx
@@ -0,0 +1,109 @@
+import { ArrowRight, ExternalLink, Github } from "lucide-react";
+
+const projects = [
+    {
+        id: 1,
+        title: "Complex Calculator",
+        description: "A complex calculator built with python",
+        image: "/projects/project1.png",
+        tags: ["Python", "Replit", "Terminal"],
+        demoUrl: "#",
+        githubUrl: "https://github.com/pedroafsnazario/Calculadora-Complexa",
+    },
+
+    {
+        id: 2,
+        title: "Quiz App",
+        description: "A quiz app built with Kotlin",
+        image: "/projects/project2.png",
+        tags: ["Kotlin", "Android", "Java"],
+        demoUrl: "#",
+        githubUrl: "https://github.com/pedroafsnazario/QUIZOFICIAL",
+
+    },
+
+    {
+        id: 3,
+        title: "Portfolio Website",
+        description: "My portfolio website built with React",
+        image: "/projects/project3.png",
+        tags: ["React", "JavaScript", "Tailwind CSS"],
+        demoUrl: "#",
+        githubUrl: "#",
+    }
+]
+
+export const ProjectsSection = () => {
+    return (
+        <section id="projects" className="py-24 px-4 relative">
+            <div className="container mx-auto max-w-5xl">
+                <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
+                    Featured <span className="text-primary"> Projects </span>
+                </h2>
+
+
+                <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+                    Here are some of my projects that I have worked on. You can find more of my work on my Github profile.
+                    Each project was a learning experience and helped me grow as a developer.
+                </p>
+
+
+                <div className="grid grid-cols-1 md:grid-cols2 lg:grid-cols-3 gap-8">
+                    {projects.map((project,key) => (
+                        <div
+                        key = {key}
+                        className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover"
+                        >
+                            <div className="h-75 overflow-hidden">
+                                <img 
+                                src={project.image} 
+                                alt={project.title} 
+                                className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+                                />
+                            </div>
+
+                            <div className="p-6">
+                                <div className="flex flex-wrap gap-2 mb-4">
+                                    {project.tags.map((tag) => (
+                                        <span className="px-2 py-1 text-xs font-medium  border rounded-full bg-secondary text-secondary-foreground">
+                                            {tag}
+                                        </span>
+                                    ))}
+                                </div>
+                            
+
+                            <h3 className="text-xl font-semibold mb-1"> {project.title}</h3>
+                            <p className="text-muted-foreground text-sm mb-4">{project.description}</p>
+                            <div className="flex justify-between items-center">
+                                <div className="flex space-x-3">
+
+                                    <a href={project.demoUrl} 
+                                    className="text-foreground/80 hover:text-primary transition-colors duration-300" 
+                                    target="_blank"
+                                    onClick={(e) => {
+                                        if (project.demoUrl === "#") {
+                                            e.preventDefault();
+                                                alert("Only available in Github!");
+                                        }
+                                    }}
+                                    > 
+                                    <ExternalLink size={20}/>
+                                    </a>
+
+                                    <a href={project.githubUrl} className="text-foreground/80 hover:text-primary transition-colors duration-300" target="_blank"> <Github size={20}/></a>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+                <div className="text-center mt-12">
+                    <a className="cosmic-button w-fit flex items-center mx-auto gap 2" href="https://github.com/pedroafsnazario" target="_blank">
+                        Check My GitHub <ArrowRight size={16}/>
+                    </a>
+                </div>
+            </div>
+        </section>
+    );
+}
